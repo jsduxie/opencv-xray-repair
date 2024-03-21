bbbbdd9127bf431c5bd5eef7a0ede1a96d57f0bc
jsduxie <james.s.duxbury@durham.ac.uk>
2024-03-21 19:24:23 +0000

    Warp + Initial Smoothing
    - Image Smoothing (bilateral filter)
    - Warp adjustment
    
    Accuracy is 0.7

--- main.py
+++ main.py
@@ -4,6 +4,9 @@
 import sys
 import os
 import cv2
+import math
+import numpy as np
+import re
 
 
 window_name_original = 'Input :('
@@ -22,20 +25,68 @@ class file_handling:
             else:
                 self.directory = sys.argv[1]
                 print(os.listdir(self.directory))
-        output_dir = "./outputs"
+                self.unfiltered_images = os.listdir(self.directory)
+
+                self.images = [file for file in self.unfiltered_images if re.match(r'.*\.(jpg|jpeg|png|gif)$', file)]
+                print(len(self.images), len(self.unfiltered_images))
+
+
+        self.output_dir = "./Results"
+    def save_image(self, image, filename):
+        cv2.imwrite(f'{self.output_dir}/{filename}', image)
+
+
 
 class image_processing:
     def __init__(self):
-        print('Hello')     
-        
         image = cv2.imread(f'./{file.directory}/im001-healthy.jpg', cv2.IMREAD_COLOR)
-        if not image is None:
+        '''def click_event(event, x, y, flags, params):
+            if event == cv2.EVENT_LBUTTONDOWN:
+                print(f'({x}, {y})')
+                cv2.putText(image, f'({x},{y})',(x,y),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                cv2.circle(image, (x,y), 1, (255, 255, 255), 1)  
 
+        
+        
+        pts_before = np.float32([[251, 229], [31, 238], [9,18], [234,8]])
+        pts_after = np.float32([[255, 255], [0, 255], [0,0], [255,0]])
+        M = cv2.getPerspectiveTransform(pts_before,pts_after)
+        dst = cv2.warpPerspective(image, M,(255,255))
+        cv2.imshow("Before", image)
+        cv2.imshow("Warped", dst)
+        
+
+        cv2.namedWindow('Point Coordinates')
+
+        cv2.setMouseCallback('Point Coordinates', click_event)
+
+        while True:
+            cv2.imshow('Point Coordinates',image)
+            k = cv2.waitKey(1) & 0xFF
+            if k == ord("x"):
+                break
+        cv2.destroyAllWindows()'''
+            
+        
+    def image_smoothing(self, image):
+        #transformed = cv2.medianBlur(image, 3)
+        transformed = cv2.bilateralFilter(image, 11, 150, 25)
+        
+        return transformed
+
+        '''
+        if not image is None:
+            x = 1
             keep_processing = True
             while (keep_processing):
                 transformed = cv2.medianBlur(image, 5)
+                transformed2 = cv2.GaussianBlur(transformed, (0,0), x)
+                
+
                 cv2.imshow(window_name_original, image)
-                cv2.imshow(window_name_transformed, transformed)
+                cv2.imshow(window_name_transformed, transformed2)
+
                 key = cv2.waitKey(0) # wait
 
                 if (key == ord('x')):
@@ -43,6 +94,31 @@ class image_processing:
                     keep_processing = False
                 elif (key == ord('v')):
                     x = x + 2
+                elif (key == ord('c')):
+                    c = c + 2
+                elif (key == ord('a')):
+                    alpha = alpha + 2'''
+    def unwarped(self, image):
+        pts_before = np.float32([[250, 227], [30, 233], [10,16], [233,6]])
+        pts_after = np.float32([[255, 255], [0, 255], [0,0], [255,0]])
+        M = cv2.getPerspectiveTransform(pts_before,pts_after)
+        dst = cv2.warpPerspective(image, M,(255,255))
+        return dst
+    def process_image(self, filename):
+        image = cv2.imread(f'./{file.directory}/{filename}', cv2.IMREAD_COLOR)
+        image = self.image_smoothing(image)
+        image = self.unwarped(image)
+        
+        return image
+
+    def process_all_images(self):
+        i = 1
+        for image in file.images:
+            print(f"Processing image {i} of {len(file.images)} - {image}")
+            i += 1
+            print(image)
+            processed_image = self.process_image(image)
+            file.save_image(processed_image, image)
 
 
 
@@ -55,4 +131,6 @@ file = file_handling()
 if (file.status == -1):
     exit()
 
+
 processing = image_processing()
+processing.process_all_images()
